ed25efd07e0451eabfed9addef8751515930e073
MarcondesOliveira <marcondescurso@outlook.com>
2020-09-27 21:31:34 -0300

    first commit

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@gmail.com',
+    avatarUrl:
+      'https://www.iconfinder.com/data/icons/avatars-xmas-giveaway/128/builder_worker_helmet-512.png',
+  },
+  {
+    id: 2,
+    name: 'Marina Aguiar',
+    email: 'marina@gmail.com',
+    avatarUrl:
+      'https://www.iconfinder.com/data/icons/avatars-xmas-giveaway/128/female_woman_avatar_portrait_1-512.png',
+  },
+  {
+    id: 3,
+    name: 'Julio Fragoso',
+    email: 'julio@gmail.com',
+    avatarUrl:
+      'https://www.iconfinder.com/data/icons/avatars-xmas-giveaway/128/man_male_avatar_portrait-512.png',
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'rafaelm@gmail.com',
+    avatarUrl:
+      'https://www.iconfinder.com/data/icons/avatars-xmas-giveaway/128/boy_afro_kid_child-512.png',
+  },
+];
